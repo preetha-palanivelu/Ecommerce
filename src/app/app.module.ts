c415349b3636682deda606f034ef3df1bc67b834
Preetha <preethajivitam@gmail.com>
2024-12-01 19:53:10 +0530

    seller page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SellerHomeComponent } from './seller-home/seller-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    SellerAuthComponent
+    SellerAuthComponent,
+    SellerHomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
 
   ],
   providers: [
--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SellerService } from '../services/seller.service';
+import { signUp } from '../data-type';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
   styleUrl: './seller-auth.component.css',
 })
 export class SellerAuthComponent implements OnInit {
-  constructor() {}
+  constructor(private seller: SellerService, private router: Router) {}
 
   ngOnInit(): void {}
-  signUp(data: object): void {
+  signUp(data: signUp): void {
     console.warn(data);
+    this.seller.userSignUp(data).subscribe((result) => {
+      if (result) {
+        this.router.navigate(['seller-home']);
+      }
+    });
   }
 }
